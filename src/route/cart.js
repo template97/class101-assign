79c05b33f7e5130da440fa7ed1125806059f08c5
template97 <gogodbwngud@naver.com>
2020-08-07 13:38:57 +0900

    initial routes setting

---
+++ src/route/cart.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Cart extends Component {
+    render () {
+        return (
+            <div> hello cart</div>
+        )
+    }
+}
+
+export default Cart;
---
+++ src/route/products.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Products extends Component {
+    render () {
+        return (
+            <div> hello Products</div>
+        )
+    }
+}
+
+export default Products;
